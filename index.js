a6b25aeb720ead092fcd2396ef36344c0da718c7
Peter Wambua <pierremumo@gmail.com>
2025-06-04 20:56:55 +0300

    Define welcomeMessage, excitedWelcomeMessage, and shortGreeting

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+console.log(welcomeMessage); // Outputs: Welcome to Flatbook, Grace Hopper!
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+console.log(welcomeMessage); // Outputs: Welcome to Flatbook, Grace Hopper!
+console.log(excitedWelcomeMessage); // Outputs: WELCOME TO FLATBOOK, GRACE HOPPER!
+const shortGreeting = `Welcome, ${currentUser.split(' ')[0]}!`;
+
+console.log(welcomeMessage); // Outputs: Welcome to Flatbook, Grace Hopper!
+console.log(excitedWelcomeMessage); // Outputs: WELCOME TO FLATBOOK, GRACE HOPPER!
